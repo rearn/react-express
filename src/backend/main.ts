55f7b3eba4745494b232d60d58d1ddffabeda96f
minamibashi rearn <minamibashi@rearn.info>
2020-09-28 21:24:04 +0900

    logging できてなかったので修正

--- src/backend/main.ts
+++ src/backend/main.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import path from 'path';
 import api2005 from '@/router/api2005';
+import { httpLogger } from '@/module/logger';
 
 const app = express();
+
+app.use(httpLogger);
 
 app.use('/', express.static(path.resolve(__dirname, 'frontend')));
 
