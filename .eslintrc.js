b4f1bd45aae4255c9da79bc1b11b8f44c075979e
minamibashi rearn <minamibashi@rearn.info>
2020-09-28 18:54:20 +0900

    lint config 修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,88 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2020": true,
-        "node": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
+  env: {
+    es2020: true,
+    node: true,
+    browser: true,
+  },
+  extends: [
+    'airbnb-typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+        json: 'never',
+        styl: 'always',
+      },
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
+
+    'import/prefer-default-export': 'off',
+
+    // typescript 関連
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+
+    // 短縮表現のため
+    'no-return-assign': 'off',
+  },
+  globals: {
+    NodeJS: true,
+  },
+  
+  overrides: [
+    {
+      files: [
+        '**/tests/**/*.test.{j,t}s(x)?',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+    {
+      files: [
+        'src/frontend/**/*.{j,t}s(x)?',
+      ],
+      settings: {
+        'import/resolver': 'webpack',
+      },
+      extends: [
+        'eslint:recommended',
+        'react-app',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:@typescript-eslint/recommended',
+      ],
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
         },
-        "ecmaVersion": 12,
-        "sourceType": "module"
+        ecmaVersion: 12,
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint",
-        "jest"
-    ],
-    "rules": {
-    }
+    {
+      files: [
+        'src/backend/**/*.{j,t}s?',
+      ],
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+      ],
+    },
+  ],
 };
